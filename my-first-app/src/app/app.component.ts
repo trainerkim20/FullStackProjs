bae6bbc0bd6f5e05ea68225bc6d30820d98f19d7
trainerkim20 <trainerkin20@gmail.com>
2020-04-29 18:29:33 -0600

    final details of ng assigment

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = '';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
