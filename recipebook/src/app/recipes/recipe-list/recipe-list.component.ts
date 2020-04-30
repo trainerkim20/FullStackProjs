f30612ff4fbbe3a202b109b68d8fb8c6d1564690
trainerkim20 <trainerkin20@gmail.com>
2020-04-29 22:00:42 -0600

    outputting list of recipes

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.mode';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-  new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+  new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+  new Recipe('A Test Recipe 2', 'this is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
 ];
   constructor() { }
 
