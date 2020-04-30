994676b2d486a1239ac1a8539fd62675cd0ee75c
trainerkim20 <trainerkin20@gmail.com>
2020-04-30 10:51:37 -0600

    added components contacts

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactsComponent } from './contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
