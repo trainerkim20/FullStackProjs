06348c1059e530e8db7df46a8d8f715e00f8331b
trainerkim20 <trainerkin20@gmail.com>
2020-04-29 22:32:21 -0600

    inputing shopping list

--- recipebook/src/app/shared/ingredient.model.ts
+++ recipebook/src/app/shared/ingredient.model.ts
@@ -1,5 +1,4 @@
 export class Ingredinet {
     constructor(public name: string, public amount: number) {
-
     }
 }
--- recipebook/src/app/shopping-list/shopping-list.component.ts
+++ recipebook/src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredinet } from '../shared/ingredient.model';
+
+
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredinet[] = [
+    new Ingredinet('Apples', 5),
+    new Ingredinet('Tomatoes', 5),
+  ];
 
   constructor() { }
 
