1cb7c1bb46d22e70249b38774e7647b44e142ff6
trainerkim20 <trainerkin20@gmail.com>
2020-05-01 11:40:55 -0600

    added group null

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -8,8 +8,8 @@ import { Contact } from '../contacts.model';
 })
 export class ContactListComponent implements OnInit {
 contacts: Contact[] = [
-  new Contact(1,'Bro. Jackson','jacksonk@byui.edu',208-496-3771,'https://web.byui.edu/Directory/Employee/jacksonk.jpg',),
-  new Contact(2,'Bro. Barzee','barzeer@byui.edu',208-496-3768,'https://web.byui.edu/Directory/Employee/barzeer.jpg',),
+  new Contact(1,'Bro. Jackson','jacksonk@byui.edu',208-496-3771,'https://web.byui.edu/Directory/Employee/jacksonk.jpg', null),
+  new Contact(2,'Bro. Barzee','barzeer@byui.edu',208-496-3768,'https://web.byui.edu/Directory/Employee/barzeer.jpg', null),
 ];
   constructor() { }
 
--- cms/src/app/contacts/contacts.model.ts
+++ cms/src/app/contacts/contacts.model.ts
@@ -4,13 +4,15 @@ export class Contact {
     public email: string;
     public phone: number;
     public imageUrl: string;
+    public group: null;
 
-    constructor(contactId: number, name: string, email: string, phone: number, imageUrl: string) {
+    constructor(contactId: number, name: string, email: string, phone: number, imageUrl: string, group: null) {
         this.name = name;
         this.contactId = contactId;
         this.imageUrl = imageUrl;
         this.email = email;
         this.phone = phone;
+        this.group = group;
 
     }
 }
