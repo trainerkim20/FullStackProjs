5140da8b5a0f4670718364c44ac3449cddce9a99
trainerkim20 <trainerkin20@gmail.com>
2020-05-01 10:56:35 -0600

    little edit

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -7,8 +7,7 @@ import { Contact } from '../contacts.model';
   styleUrls: ['./contact-detail.component.css']
 })
 export class ContactDetailComponent implements OnInit {
-  contacts: Contact[] = [
-    new Contact(1,'Bro. Jackson','jacksonk@byui.edu',208-496-3771,'https://web.byui.edu/Directory/Employee/jacksonk.jpg',) ];
+  contacts: Contact[] = [];
   
     constructor() { }
 
