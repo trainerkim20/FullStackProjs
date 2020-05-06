35e27b163e3c13219ea955cd18b355e141fcba86
trainerkim20 <trainerkin20@gmail.com>
2020-05-06 16:10:36 -0600

    Lifestyle Hooks in Action

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestory } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +6,37 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'], 
   encapsulation: ViewEncapsulation.Emulated // None, Native
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestory {
   @Input() element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
+  constructor() { 
+    console.log('constructor called!');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges() {
+console.log('ngOnChanges called!')
+console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('Do Check called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('AfterContent called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked called!');
+  }
+
+  ngOnDestory() {
+    console.log('OnDestory called!');
   }
 
 }
