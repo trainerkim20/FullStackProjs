92ccfe76e4b39f0a5fc39af25f7172fc1953ba7a
trainerkim20 <trainerkin20@gmail.com>
2020-05-06 17:13:54 -0600

    added navigation

--- recipebook/src/app/app.component.ts
+++ recipebook/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipebook';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
