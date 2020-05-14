ee1fd794878b18fbabc60cdc79268172bbf99555
trainerkim20 <trainerkin20@gmail.com>
2020-05-14 10:14:42 -0600

    documentItemCompenent

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+
+@Input() document: Document;
 
   constructor() { }
 
