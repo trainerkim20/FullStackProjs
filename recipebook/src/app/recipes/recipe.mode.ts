6591399de02ef54ca7b3c168b7833522334b5f12
trainerkim20 <trainerkin20@gmail.com>
2020-05-20 16:19:46 -0600

    adding ingrediants to recipes

--- recipebook/src/app/recipes/recipe.mode.ts
+++ recipebook/src/app/recipes/recipe.mode.ts
@@ -1,12 +1,16 @@
+import { Ingredinet } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredinet[];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredinet[]) {
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
 
     }
 }
--- recipebook/src/app/recipes/recipe.service.ts
+++ recipebook/src/app/recipes/recipe.service.ts
@@ -1,11 +1,21 @@
 import { Recipe } from './recipe.mode';
 import { EventEmitter } from '@angular/core';
+import { Ingredinet } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe('A Test Recipe 2', 'this is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('Schnitzel', 'Awesome', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg', 
+        [
+            new Ingredinet('Meat', 1),
+            new Ingredinet('French Fries', 20),
+
+        ]),
+        new Recipe('Burger', 'Greasy', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg', 
+        [
+            new Ingredinet('Buns', 2),
+            new Ingredinet('Meat', 1),
+        ]),
       ];
 
       getRecipes() {
