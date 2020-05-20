9a05cce036168a7c54da8155c7da5daa44b1d827
trainerkim20 <trainerkin20@gmail.com>
2020-05-20 15:47:21 -0600

    recipe selection works with service

--- recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipebook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.mode';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,18 @@ import { Recipe } from '../recipe.mode';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-    new Recipe('A Test Recipe 2', 'this is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-  ];
+  recipes: Recipe[];
   
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  // @Output() recipeWasSelected = new EventEmitter<Recipe>();
   
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
+  // onRecipeSelected(recipe: Recipe) {
+  //   this.recipeWasSelected.emit(recipe);
+  // }
 
 }
--- recipebook/src/app/recipes/recipe.service.ts
+++ recipebook/src/app/recipes/recipe.service.ts
@@ -1,3 +1,14 @@
+import { Recipe } from './recipe.mode';
+import { EventEmitter } from '@angular/core';
+
 export class RecipeService {
-    
+    recipeSelected = new EventEmitter<Recipe>();
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'this is simply a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('A Test Recipe 2', 'this is simply a test 2', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+      ];
+
+      getRecipes() {
+          return this.recipes.slice();
+      }
 }
