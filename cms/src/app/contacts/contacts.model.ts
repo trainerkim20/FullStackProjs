8356814e3cd8d2e9e946249d459ea7202a26bcb9
trainerkim20 <trainerkin20@gmail.com>
2020-05-21 22:21:07 -0600

    new error with contacts model

--- cms/src/app/contacts/contacts.model.ts
+++ cms/src/app/contacts/contacts.model.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class Contact {
-    // id: string;
-    // public name: string;
-    // public email: string;
-    // public phone: string;
-    // public imageUrl: string;
-    // public group: Contact[];
+    public id: string;
+    public name: string;
+    public email: string;
+    public phone: string;
+    public imageUrl: string;
+    public group: Contact[];
 
-    constructor(public id: string,public name: string,public email: string,public phone: string,public imageUrl: string,public group: Contact[]) {
+    constructor( id: string, name: string, email: string, phone: string, imageUrl: string, group: Contact[]) {
         this.name = name;
         this.id = id;
         this.imageUrl = imageUrl;
