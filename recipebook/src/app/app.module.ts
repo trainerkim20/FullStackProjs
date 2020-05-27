ea05238554f9492febe89d903826b20299e505b9
trainerkim20 <trainerkin20@gmail.com>
2020-05-26 22:24:48 -0600

    setting up routes

--- recipebook/src/app/app.module.ts
+++ recipebook/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-it
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { ShoppingListService } from './shopping-list/shopping-list.service'
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { ShoppingListService } from './shopping-list/shopping-list.service'
   imports: [
     BrowserModule,
     FormsModule,
+    AppRoutingModule
    
   ],
   providers: [ShoppingListService],
