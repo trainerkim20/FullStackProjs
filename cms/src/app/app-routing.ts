6065cdfe7eb29f1306ae2760ef46b18c6f9d6d6f
trainerkim20 <trainerkin20@gmail.com>
2020-05-28 14:54:10 -0600

    add document router

--- cms/src/app/app-routing.ts
+++ cms/src/app/app-routing.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DocumentsComponent } from './documents/documents.component';
 import { MessagesComponent} from './messages/messages.component';
-import { ContactsComponent } from './contacts/contacts.component'
+import { ContactsComponent } from './contacts/contacts.component';
+import { DocumentsEditComponent } from './documents/documents-edit/documents-edit.component';
+import { DocumentDetailComponent } from './documents/document-detail/document-detail.component'
 
 const app_Routes: Routes = [
     { path: '', redirectTo: '/documents', pathMatch: 'full'},
-    { path: 'documents', component: DocumentsComponent },
+    { path: 'documents', component: DocumentsComponent, children: [
+        { path: 'new', component: DocumentsEditComponent},
+        { path: ':id', component: DocumentDetailComponent},
+        { path: ':id/edit', component: DocumentsEditComponent},
+    ] },
     { path: 'messages', component: MessagesComponent },
     { path: 'contact', component: ContactsComponent },
 ];
--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Message } from '../message.model';
 import { MessagesService} from '../messages.service'
 
