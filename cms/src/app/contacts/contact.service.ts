1e71b8bbf71d786fa22c7215eddee6e79f79ad12
trainerkim20 <trainerkin20@gmail.com>
2020-06-04 22:54:13 -0600

    add, delete and update functions are in

--- cms/src/app/contacts/contact.service.ts
+++ cms/src/app/contacts/contact.service.ts
@@ -14,8 +14,26 @@ export class ContactService {
 
   contacts: Contact[] = [];
 
+  maxContactId: number;
+
   constructor() {
     this.contacts = MOCKCONTACTS;
+    this.maxContactId = this.getMaxId();
+   }
+
+   addContact(newContact: Contact) {
+     if (!newContact) {
+       return;
+     }
+     this.maxContactId++;
+
+     newContact.id = this.maxContactId.toString();
+
+     this.contacts.push(newContact);
+
+     const contactListClone = this.contacts.slice();
+
+     this.contactListChangedEvent.next(contactListClone);
    }
 
     deleteContact(contact: Contact) {
@@ -29,7 +47,10 @@ export class ContactService {
     }
 
     this.contacts.splice(pos, 1);
-    this.contactChangedEvent.emit(this.contacts.slice());
+    const contactListClone = this.contacts.slice();
+
+     this.contactListChangedEvent.next(contactListClone);
+    // this.contactChangedEvent.emit(this.contacts.slice());
   }
 
    getContacts() {
@@ -43,5 +64,39 @@ export class ContactService {
        }
      }
      return null;
+   }
+
+   getMaxId(): number {
+    let maxId = 0;
+
+    for (const contact of this.contacts) {
+      let currentId = parseInt(contact.id);
+
+      if (currentId > maxId) {
+        maxId = currentId;
+      }
+    }
+    return maxId;
+   }
+
+   updateContact(originalContact: Contact, newContact: Contact) {
+     if (!originalContact || !newContact) {
+       return;
+     }
+
+     const pos = this.contacts.indexOf(originalContact);
+
+     if(pos < 0) {
+       return;
+     }
+
+     newContact.id = originalContact.id;
+
+     this.contacts[pos] = newContact;
+
+     const contactListClone = this.contacts.slice();
+
+     this.contactListChangedEvent.next(contactListClone);
+
    }
 }
--- cms/src/app/documents/documents.service.ts
+++ cms/src/app/documents/documents.service.ts
@@ -22,6 +22,24 @@ export class DocumentsService {
     this.maxDocumentId = this.getMaxId();
    }
 
+   maxDocumentId: number;
+
+   addDocument(newDocument: Document) {
+    if (!newDocument) {
+      return;
+    }
+
+    this.maxDocumentId++;
+
+    newDocument.id = this.maxDocumentId.toString();
+
+    this.documents.push(newDocument);
+
+    const documentListClone = this.documents.slice();
+
+    this.documentListChangedEvent.next(documentListClone);
+  }
+
    deleteDocument(document: Document) {
     if (document === null) {
       return;
@@ -33,7 +51,10 @@ export class DocumentsService {
     }
 
     this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit(this.documents.slice());
+    const documentListClone = this.documents.slice();
+
+    this.documentListChangedEvent.next(documentListClone);
+    // this.documentChangedEvent.emit(this.documents.slice());
   }
 
    getDocuments() {
@@ -61,23 +82,34 @@ getMaxId(): number {
     }
   }
   return maxId;
-  }  
+  } 
+  
+  updateDocument(originalDocument: Document, newDocument: Document) {
+    if (!originalDocument || !newDocument) {
+      return;
+    }
+
+    const pos = this.documents.indexOf(originalDocument);
+
+    if(pos < 0) {
+      return;
+    }
+
+    newDocument.id = originalDocument.id;
+
+    this.documents[pos] = newDocument;
+
+    const documentListClone = this.documents.slice();
+
+    this.documentListChangedEvent.next(documentListClone);
+
+  }
+
+
+
 }
 
-// addDocument(newDocument: Document) {
-  
-//   if (!newDocument) {
 
-//   }
   
-  
-  // if newDocument is undefined or null then
-  //     return
-  // endIf
 
-  // this.maxDocumentId++
-  // newDocument.id = this.maxDocumentId
-  // push newDocument onto the documents list
-  // documentsListClone = documents.slice()
-  // documentListChangedEvent.next(documentsListClone)
-}
+
