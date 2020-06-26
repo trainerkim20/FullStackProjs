de6478304e440453acdd20408b592efeb29c2fd1
trainerkim20 <trainerkin20@gmail.com>
2020-06-25 21:35:09 -0600

    adding post backend point

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -10,7 +14,16 @@ app.use((req, res, next) => {
      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
      );
     next();
-}) 
+});
+
+app.post("/api/posts", (req, res, next) => {
+    const posts = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Psot added successfully!'
+    });
+});
+
 
 // app.use((req, res, next) => {
 //     console.log('First middleware')
