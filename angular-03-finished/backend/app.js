5576850c9eba401c2412f58b7106876d16dc10a0
trainerkim20 <trainerkin20@gmail.com>
2020-06-25 21:43:21 -0600

    adding angular

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Header",
+    res.setHeader("Access-Control-Allow-Headers",
      "Origin, X-Requested-With, Content-Type, Accept"
      );
      res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS"
@@ -30,7 +30,7 @@ app.post("/api/posts", (req, res, next) => {
 // next();
 // });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
     const posts = [
         { id: 'fondjdnkdk', 
         title: 'First server-side post', 
