caab45ec6a2fb4b339da34799e1eac2ea59e5dbd
trainerkim20 <trainerkin20@gmail.com>
2020-06-30 17:50:55 -0600

    fetch

--- angular-03-finished/backend/app.js
+++ angular-03-finished/backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const Post = require('./models/post');
 
 const app = express();
+
+//insert passwrd and SRV to mongo
+mongoose.connect()
+.then(() => {
+    console.log('Connected to database');
+}) 
+.catch(() => {
+    console.log('Connection failed');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -23,7 +33,7 @@ app.post("/api/posts", (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     });
-    console.log(post);
+    post.save();
     res.status(201).json({
         message: 'Psot added successfully!'
     });
@@ -37,18 +47,21 @@ app.post("/api/posts", (req, res, next) => {
 
 
 app.get('/api/posts', (req, res, next) => {
-    const posts = [
-        { id: 'fondjdnkdk', 
-        title: 'First server-side post', 
-        content: 'This is coming from the server'},
+    Post.find().then(documents => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: documents
+    });
+    // const posts = [
+    //     { id: 'fondjdnkdk', 
+    //     title: 'First server-side post', 
+    //     content: 'This is coming from the server'},
 
-        { id: 'cneighkebrw', 
-        title: 'Second server-side post', 
-        content: 'This is coming from the server!'}
-    ];
-    res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: posts
+    //     { id: 'cneighkebrw', 
+    //     title: 'Second server-side post', 
+    //     content: 'This is coming from the server!'}
+    // ];
+    
     });
 
     });
