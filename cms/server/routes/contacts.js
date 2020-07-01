e817dfe1139549156fbaf1f721864a6c4ab0ea5f
trainerkim20 <trainerkin20@gmail.com>
2020-06-30 23:19:53 -0600

    contact router and module?

--- cms/server/routes/contacts.js
+++ cms/server/routes/contacts.js
@@ -11,4 +11,104 @@ function returnError(res, error) {
     });
 }
 
-router.get('')
+router.get('/', (req, res, next) => {
+    Contact.find()
+    .population('group')
+    .then(contacts => {
+        res.status(200).json({
+            message: 'Contacts fetched successfully!',
+            contacts: contacts
+        });
+    })
+    .catch(error => {
+        returnError(res, error);
+    });
+});
+
+router.get('/:id', (req, res, next) => {
+    Contact.findOne({
+        "id": req.params.id
+    })
+    .populate('group')
+    .then(contact => {
+        res.status(200).json({
+            message: 'Contact fetched successfully!',
+            contact: contact
+        });
+    })
+    .catch(error => {
+        returnError(res, error);
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const maxContactId = sequenceGenerator.nextId("contacts");
+
+    const contact = new Contact({
+        id: maxContactId,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone,
+        imageUrl: req.body.imageUrl
+    });
+
+    contact.save()
+    .then(createdContact => {
+        res.status(201).json({
+            message: "Contact added successfully!",
+            contact: createdContact
+        });
+    })
+    .catch(error => {
+        returnError(res, error);
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+    Contact.findOne({
+        id: req.params.id
+    })
+    .then(contact => {
+        contact.name = req.body.name;
+        contact.email = req.body.email;
+        contact.phone = req.body.phone;
+        contact.imageUrl = req.body.imageUrl;
+        contact.group = req.body.group;
+
+        Contact.updateOne({
+            id: req.params.id
+        }, contact)
+        .then(result => {
+            res.status(204).json({
+                message: 'Contact updated successfully!'
+            })
+        })
+        .catch(error => {
+            returnError(res, error);
+        });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+    Contact.findOne({
+        id: req.params.id
+    })
+    .then(contact => {
+        Contact.deleteOne({
+            id: req.params.id
+        })
+        .then(result => {
+            res.status(204).json({
+                message: "Contact deleted successfully!"
+            });
+        })
+        .catch(error => {
+            returnError(res, error);
+        })
+    })
+    .catch(error => {
+        returnError(res, error);
+    });
+});
+
+module.exports = router;
