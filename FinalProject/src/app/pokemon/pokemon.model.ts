6623e768301ba279ecb36f931bf740a75e11fecc
trainerkim20 <trainerkin20@gmail.com>
2020-07-08 18:01:05 -0600

    added nickname but removed region

--- FinalProject/src/app/pokemon/pokemon.model.ts
+++ FinalProject/src/app/pokemon/pokemon.model.ts
@@ -9,7 +9,7 @@ export class Pokemon {
     // public imageUrl: string;
     // public group: Contact[];
 
-    constructor(public id: string, public name: string, public type: string, public game: string, public region: string, public imageUrl: string) {
+    constructor(public id: string, public name: string, public nickname: string, public type: string, public game: string, public imageUrl: string) {
         
         // this.id = id;
         // this.name = name;
